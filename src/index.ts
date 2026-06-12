981938ac743d3ff41ebaf57aa9bb199f4400fa66
EMG <morenoelias@icloud.com>
2026-06-12 16:24:02 -0400

    chore: preparar backend para desplegar en Render con Blueprint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
 import categoriesRouter from './routes/categories.routes.js'
@@ -12,6 +13,7 @@ const app = new Hono()
 
 // Middleware globales
 app.use('*', logger())
+app.use('*', cors())
 
 // Servir archivos estáticos
 app.use('/uploads/*', serveStatic({ root: './' }))
